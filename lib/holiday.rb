848731e456669a685f105d9297dca208cdb9a6f4
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 14:38:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash do |season, holiday|
+holiday_hash.each do |season, holiday|
   if holiday.include?("BBQ")
     return holiday
   end
