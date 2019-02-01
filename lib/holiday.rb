d1cd123e5205fb71bde5e4b0912bf7a094157a10
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 14:42:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 bbq = []
 holiday_hash.each do |season, holiday|
+  holiday.each do |
   if holiday.include?("BBQ")
     bbq << holiday
   end
