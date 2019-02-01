2cbaeef352020d8cd563ce15a1018f6b3e065523
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 14:18:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, data|
   puts "#{season.capitalize}:"
   data.each do | holiday, supply|
-    supply.each { |item| item.join(, ) }
+    supply.each { |item| item.join(" ") }
     puts "#{holiday.capitalize}: #{item}"
   end
   end
